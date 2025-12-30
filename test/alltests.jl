c25de7a73850a70174c2446a5646263998683913
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2025-12-30 22:23:17 +0100

    Remove superfluous tests

--- test/alltests.jl
+++ test/alltests.jl
@@ -1,7 +1,6 @@
 using LinearAlgebra
 using SparseArrays
 using ExtendableSparse
-#using ExtendableSparse.Experimental
 using Printf
 using BenchmarkTools
 
@@ -31,16 +30,6 @@ end
         end
     end
 end
-
-# @testset "ExperimentalParallel" begin
-#     include("ExperimentalParallel.jl")
-#     for d=[1,2,3]
-#         for N in [10,rand(30:40),50]
-#             ExperimentalParallel.test_correctness_build(N,d)
-#         end
-#     end
-# end
-
 
 @testset "Constructors" begin
     include("test_constructors.jl")
@@ -82,40 +71,10 @@ end
     include("test_linearsolve.jl")
 end
 
-# @testset "Preconditioners" begin
-#     include("test_preconditioners.jl")
-# end
-
 @testset "Symmetric" begin
     include("test_symmetric.jl")
 end
 
-# @testset "ExtendableSparse.LUFactorization" begin
-#     include("test_default_lu.jl")
-# end
-
-# @testset "Sparspak" begin
-#     include("test_sparspak.jl")
-# end
-
-# if ExtendableSparse.USE_GPL_LIBS
-#     @testset "Cholesky" begin
-#         include("test_default_cholesky.jl")
-#     end
-# end
-
 @testset "block" begin
     include("test_block.jl")
 end
-
-#@testset "parilu0" begin include("test_parilu0.jl") end
-
-
-# @testset "mkl-pardiso" begin if !Sys.isapple()
-#     include("test_mklpardiso.jl")
-# end end
-
-
-# if Pardiso.PARDISO_LOADED[]
-#      @testset "pardiso" begin include("test_pardiso.jl") end
-# end
