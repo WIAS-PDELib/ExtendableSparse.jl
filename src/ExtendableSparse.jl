4dcd7b4b420c61b374501a7ebb5137555c1eade7
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2025-12-30 22:18:17 +0100

    Remove factorizations

--- src/ExtendableSparse.jl
+++ src/ExtendableSparse.jl
@@ -2,23 +2,17 @@ module ExtendableSparse
 
 using DocStringExtensions: DocStringExtensions, SIGNATURES, TYPEDEF, TYPEDFIELDS
 using ILUZero: ILUZero, ldiv!, nnz
-using LinearAlgebra: LinearAlgebra, Diagonal, Hermitian, Symmetric, Tridiagonal,
-    cholesky, cholesky!, convert, lu!, mul!, norm, transpose
+using LinearAlgebra: LinearAlgebra, Diagonal, Hermitian, Symmetric, Tridiagonal, convert, mul!, norm
 using SparseArrays: SparseArrays, AbstractSparseMatrix, SparseMatrixCSC,
     dropzeros!, findnz, nzrange, sparse, spzeros
-using Sparspak: Sparspak, sparspaklu, sparspaklu!
+using Sparspak: sparspaklu
 using StaticArrays: StaticArrays, SMatrix, SVector
-using SuiteSparse: SuiteSparse
 import SparseArrays: AbstractSparseMatrixCSC, rowvals, getcolptr, nonzeros
 
 
 # Define our own constant here in order to be able to
 # test things at least a little bit..
 const USE_GPL_LIBS = Base.USE_GPL_LIBS
-
-if USE_GPL_LIBS
-    using SuiteSparse
-end
 
 
 include("compat.jl") # @public
@@ -28,16 +22,8 @@ include("matrix/abstractsparsematrixextension.jl")
 include("matrix/sparsematrixlnk.jl")
 include("matrix/sparsematrixdilnkc.jl")
 include("matrix/abstractextendablesparsematrixcsc.jl")
-include("matrix/extendable.jl")
 include("matrix/genericmtextendablesparsematrixcsc.jl")
 include("matrix/genericextendablesparsematrixcsc.jl")
-
-"""
-    ExtendableSparseMatrix
-
-Aliased to [`ExtendableSparseMatrixCSC`](@ref)
-"""
-const ExtendableSparseMatrix = ExtendableSparseMatrixCSC
 
 
 """
@@ -59,8 +45,58 @@ Single threaded extendable sparse matrix (Experimental).
 Aliased to [`GenericExtendableSparseMatricCSC`](@ref) with [`SparseMatrixDILNKC`](@ref) 
 scalar matrix parameter.
 """
-const STExtendableSparseMatrixCSC{Tv, Ti} = GenericExtendableSparseMatrixCSC{SparseMatrixDILNKC{Tv, Ti}, Tv, Ti}
-STExtendableSparseMatrixCSC(m, n, args...) = STExtendableSparseMatrixCSC{Float64, Int64}(m, n, args...)
+
+const STExtendableSparseMatrixCSC{Tv, Ti} = GenericExtendableSparseMatrixCSC{SparseMatrixLNK{Tv, Ti}, Tv, Ti}
+STExtendableSparseMatrixCSC(::Type{Tv}, m::Number, n::Number) where {Tv} = STExtendableSparseMatrixCSC{Tv, Int64}(m, n)
+STExtendableSparseMatrixCSC(m::Number, n::Number) = STExtendableSparseMatrixCSC{Float64, Int64}(m, n)
+function STExtendableSparseMatrixCSC(A::AbstractSparseMatrixCSC{Tv, Ti}) where {Tv, Ti <: Integer}
+    return GenericExtendableSparseMatrixCSC(
+        SparseMatrixCSC(A),
+        SparseMatrixLNK{Tv, Ti}(size(A)...)
+    )
+end
+STExtendableSparseMatrixCSC(D::Diagonal) = STExtendableSparseMatrixCSC(sparse(D))
+STExtendableSparseMatrixCSC(I, J, V::AbstractVector) = STExtendableSparseMatrixCSC(sparse(I, J, V))
+STExtendableSparseMatrixCSC(I, J, V::AbstractVector, m, n) = STExtendableSparseMatrixCSC(sparse(I, J, V, m, n))
+STExtendableSparseMatrixCSC(I, J, V::AbstractVector, combine::Function) = STExtendableSparseMatrixCSC(sparse(I, J, V, combine))
+STExtendableSparseMatrixCSC(I, J, V::AbstractVector, m, n, combine::Function) = STExtendableSparseMatrixCSC(sparse(I, J, V, m, n, combine))
+
+
+const ExtendableSparseMatrixCSC = STExtendableSparseMatrixCSC
+const ExtendableSparseMatrix = STExtendableSparseMatrixCSC
+
+"""
+    pointblock(matrix,blocksize)
+
+Create a pointblock matrix.
+"""
+function pointblock(A0::ExtendableSparseMatrixCSC{Tv, Ti}, blocksize) where {Tv, Ti}
+    A = SparseMatrixCSC(A0)
+    colptr = A.colptr
+    rowval = A.rowval
+    nzval = A.nzval
+    n = A.n
+    block = zeros(Tv, blocksize, blocksize)
+    nblock = n ÷ blocksize
+    b = SMatrix{blocksize, blocksize}(block)
+    Tb = typeof(b)
+    Ab = ExtendableSparseMatrixCSC{Tb, Ti}(nblock, nblock)
+
+
+    for i in 1:n
+        for k in colptr[i]:(colptr[i + 1] - 1)
+            j = rowval[k]
+            iblock = (i - 1) ÷ blocksize + 1
+            jblock = (j - 1) ÷ blocksize + 1
+            ii = (i - 1) % blocksize + 1
+            jj = (j - 1) % blocksize + 1
+            block[ii, jj] = nzval[k]
+            rawupdateindex!(Ab, +, SMatrix{blocksize, blocksize}(block), iblock, jblock)
+            block[ii, jj] = zero(Tv)
+        end
+    end
+    return flush!(Ab)
+end
 
 
 export ExtendableSparseMatrixCSC, MTExtendableSparseMatrixCSC, STExtendableSparseMatrixCSC, GenericMTExtendableSparseMatrixCSC
@@ -69,12 +105,6 @@ export partitioning!
 
 export eliminate_dirichlet, eliminate_dirichlet!, mark_dirichlet
 
-include("factorizations/factorizations.jl")
-
-#include("experimental/Experimental.jl")
-
-include("factorizations/simple_iteration.jl")
-export simple, simple!
 
 include("preconbuilders.jl")
 export LinearSolvePreconBuilder, BlockPreconBuilder, JacobiPreconBuilder
@@ -86,155 +116,6 @@ include("matrix/sprand.jl")
 export sprand!, sprand_sdd!, fdrand, fdrand!, fdrand_coo, solverbenchmark
 
 export rawupdateindex!, updateindex!
-
-
-#####
-# All of the following is deprecated in favor of the precs bases
-# API
-
-export JacobiPreconditioner,
-    ILU0Preconditioner,
-    ILUZeroPreconditioner,
-    PointBlockILUZeroPreconditioner,
-    ParallelJacobiPreconditioner,
-    ParallelILU0Preconditioner,
-    BlockPreconditioner, allow_views
-
-export AbstractFactorization, LUFactorization, CholeskyFactorization, SparspakLU
-export issolver
-export factorize!, update!
-
-"""
-```
-ILUTPreconditioner(;droptol=1.0e-3)
-ILUTPreconditioner(matrix; droptol=1.0e-3)
-```
-
-Create the [`ILUTPreconditioner`](@ref) wrapping the one 
-from [IncompleteLU.jl](https://github.com/haampie/IncompleteLU.jl)
-For using this, you need to issue `using IncompleteLU`.
-"""
-function ILUTPreconditioner end
-export ILUTPreconditioner
-
-"""
-```
-AMGPreconditioner(;max_levels=10, max_coarse=10)
-AMGPreconditioner(matrix;max_levels=10, max_coarse=10)
-```
-
-Create the  [`AMGPreconditioner`](@ref) wrapping the Ruge-Stüben AMG preconditioner from [AlgebraicMultigrid.jl](https://github.com/JuliaLinearAlgebra/AlgebraicMultigrid.jl)
-
-!!! warning
-     Deprecated in favor of [`RS_AMGPreconditioner`](@ref)
-
-"""
-function AMGPreconditioner end
-export AMGPreconditioner
-
-@deprecate AMGPreconditioner() RS_AMGPreconditioner()
-@deprecate AMGPreconditioner(A) RS_AMGPreconditioner(A)
-
-"""
-```
-RS_AMGPreconditioner(;kwargs...)
-RS_AMGPreconditioner(matrix;kwargs...)
-```
-
-Create the  [`RS_AMGPreconditioner`](@ref) wrapping the Ruge-Stüben AMG preconditioner from [AlgebraicMultigrid.jl](https://github.com/JuliaLinearAlgebra/AlgebraicMultigrid.jl)
-For `kwargs` see there.
-"""
-function RS_AMGPreconditioner end
-export RS_AMGPreconditioner
-
-
-"""
-```
-SA_AMGPreconditioner(;kwargs...)
-SA_AMGPreconditioner(matrix;kwargs...)
-```
-
-Create the  [`SA_AMGPreconditioner`](@ref) wrapping the smoothed aggregation AMG preconditioner from [AlgebraicMultigrid.jl](https://github.com/JuliaLinearAlgebra/AlgebraicMultigrid.jl)
-For `kwargs` see there.
-"""
-function SA_AMGPreconditioner end
-export SA_AMGPreconditioner
-
-
-"""
-```
-AMGCL_AMGPreconditioner(;kwargs...)
-AMGCL_AMGPreconditioner(matrix;kwargs...)
-```
-
-Create the  [`AMGCL_AMGPreconditioner`](@ref) wrapping AMG preconditioner from [AMGCLWrap.jl](https://github.com/j-fu/AMGCLWrap.jl)
-For `kwargs` see there.
-"""
-function AMGCL_AMGPreconditioner end
-export AMGCL_AMGPreconditioner
-
-
-"""
-```
-AMGCL_RLXPreconditioner(;kwargs...)
-AMGCL_RLXPreconditioner(matrix;kwargs...)
-```
-
-Create the  [`AMGCL_RLXPreconditioner`](@ref) wrapping RLX preconditioner from [AMGCLWrap.jl](https://github.com/j-fu/AMGCLWrap.jl)
-"""
-function AMGCL_RLXPreconditioner end
-export AMGCL_RLXPreconditioner
-
-
-"""
-```
-PardisoLU(;iparm::Vector, 
-           dparm::Vector, 
-           mtype::Int)
-
-PardisoLU(matrix; iparm,dparm,mtype)
-```
-
-LU factorization based on pardiso. For using this, you need to issue `using Pardiso`
-and have the pardiso library from  [pardiso-project.org](https://pardiso-project.org) 
-[installed](https://github.com/JuliaSparse/Pardiso.jl#pardiso-60).
-
-The optional keyword arguments `mtype`, `iparm`  and `dparm` are 
-[Pardiso internal parameters](https://github.com/JuliaSparse/Pardiso.jl#readme).
-
-Forsetting them, one can also access the `PardisoSolver` e.g. like
-```
-using Pardiso
-plu=PardisoLU()
-Pardiso.set_iparm!(plu.ps,5,13.0)
-```
-"""
-function PardisoLU end
-export PardisoLU
-
-
-"""
-```
-MKLPardisoLU(;iparm::Vector, mtype::Int)
-
-MKLPardisoLU(matrix; iparm, mtype)
-```
-
-LU factorization based on pardiso. For using this, you need to issue `using Pardiso`.
-This version  uses the early 2000's fork in Intel's MKL library.
-
-The optional keyword arguments `mtype` and `iparm`  are  
-[Pardiso internal parameters](https://github.com/JuliaSparse/Pardiso.jl#readme).
-
-For setting them you can also access the `PardisoSolver` e.g. like
-```
-using Pardiso
-plu=MKLPardisoLU()
-Pardiso.set_iparm!(plu.ps,5,13.0)
-```
-"""
-function MKLPardisoLU end
-export MKLPardisoLU
 
 
 end # module
